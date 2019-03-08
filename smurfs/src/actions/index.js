64c8ddd7eb4c3702471934b42e328fd10088cda3
McTrip777 <mcjakedude@gmail.com>
2019-03-08 08:28:33 -0800

    Reducer and action built commit 2

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,13 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from "axios";
 
+const url = 'http://localhost:3333/smurfs'
+
+export const READ_SMURFS_START = 'READ_SMURFS_START';
+export const READ_SMURFS_SUCCESS = 'READ_SMURFS_SUCCESS';
+export const READ_SMURFS_FAIL = 'READ_SMURFS_FAIL';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +19,14 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: READ_SMURFS_START})
+  axios
+    .get(url)
+    .then(res => dispatch({type: READ_SMURFS_SUCCESS, payload: res.data}))
+    .catch(err => dispatch({type: READ_SMURFS_FAIL, payload: err}))
+}
+
+// export const CREATE_SMURFS_START = 'CREATE_SMURFS_START';
+// export const CREATE_SMURFS_SUCCESS = 'CREATE_SMURFS_SUCCESS';
+// export const CREATE_SMURFS_FAIL = 'CREATE_SMURFS_FAIL';
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,14 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  READ_SMURFS_START,
+  READ_SMURFS_SUCCESS,
+  READ_SMURFS_FAIL,
+  // CREATE_SMURFS_START,
+  // CREATE_SMURFS_SUCCESS,
+  // CREATE_SMURFS_FAIL,
+} from "../actions";
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,6 +21,14 @@
    error: null
  }
 */
+const initialState = {
+   smurfs: [],
+   fetchingSmurfs: false,
+   addingSmurf: false,
+  //  updatingSmurf: false,
+  //  deletingSmurf: false,
+   error: null,
+}
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +37,30 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case READ_SMURFS_START:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: null
+      }
+      case READ_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, ...action.payload],
+        fetchingSmurfs: false,
+        error: null
+      }
+      case READ_SMURFS_FAIL:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload,
+      }
+
+ 
+  default:
+  return state;
+}
+};
